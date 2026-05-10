274b885dd8e9c6e1a855adbfe183aff332324e2d
Naya-Nay-ai <naya4700@gmail.com>
2026-05-11 02:03:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const BACK_IMAGE_PATH = 'assets/back/back_mirror_tarot_adopted.png';
 
 const elements = {
   drawButton: document.getElementById('drawButton'),
+  deckPreview: document.getElementById('deckPreview'),
   result: document.getElementById('result'),
   cardImage: document.getElementById('cardImage'),
   cardName: document.getElementById('cardName'),
